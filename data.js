29d50dc27120dd7d70613a2e071b4cb6dec2a520
yousefkp2010-afk <yousefkp2010@gmail.com>
2026-02-25 22:10:05 +0400

    تحديث بيانات العبادات

--- data.js
+++ data.js
@@ -56,7 +56,7 @@ var appData = [
         "dhuhr": "في وقته",
         "asr": "في وقته",
         "maghrib": "في وقته",
-        "isha": "",
+        "isha": "جماعة",
         "pages": 0
       },
       "8": {
